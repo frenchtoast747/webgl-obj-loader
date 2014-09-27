537902aeb4bbecda698f58925c6d750a49553f4f
Aaron Boman <aaron@aaronboman.com>
2014-09-27 10:57:28 -0500

    Remove the startswith function as it is no longer necessary.

--- webgl-obj-loader.js
+++ webgl-obj-loader.js
@@ -1,12 +1,5 @@
 (function (window, undefined) {
   'use strict';
-  // Thanks to CMS for the startsWith function
-  // http://stackoverflow.com/questions/646628/javascript-startswith/646643#646643
-  if (typeof String.prototype.startsWith !== 'function') {
-    String.prototype.startsWith = function (str) {
-      return this.slice(0, str.length) === str;
-    };
-  }
   var OBJ = {};
   window.OBJ = OBJ;
 
