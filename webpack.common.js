6f5fba0f234aa87d900bf2b57b06b550a3ce1993
Aaron Boman <aaron@aaronboman.com>
2019-10-25 21:18:25 -0500

    Fix support for node.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,29 +1,31 @@
-const path = require('path');
+const path = require("path");
 
-const OUTDIR = path.join(__dirname, '/dist');
+const OUTDIR = path.join(__dirname, "/dist");
 
 module.exports = {
-    mode: 'none',
-    entry: [
-        path.join(__dirname, '/src/index.ts')
-    ],
+    mode: "none",
+    entry: [path.join(__dirname, "/src/index.ts")],
     output: {
         path: OUTDIR,
-        publicPath: '/',
-        library: 'OBJ',
-        libraryTarget: 'umd',
+        publicPath: "/",
+        library: "OBJ",
+        libraryTarget: "umd",
         umdNamedDefine: true,
+        // it seems as though outputting `umd` should not dump `window`,
+        // but it does... which breaks node.js projects. Explicitly
+        // set the global object here.
+        globalObject: "typeof self !== 'undefined' ? self : this",
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'awesome-typescript-loader',
+                use: "awesome-typescript-loader",
                 exclude: /node_modules/,
             },
-        ]
+        ],
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js']
+        extensions: [".tsx", ".ts", ".js"],
     },
 };
